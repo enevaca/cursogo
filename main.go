d199c05be1e45df1d7e55222494be72f76a3599f
Noel Vaca <noel.vm.2501@gmail.com>
2018-02-23 19:26:24 -0400

    For, break

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
   getArray()
   getSlice()
   ifTest()
+  forTest()
 }
 
 func getName() string {
@@ -76,4 +77,25 @@ func ifTest() {
 	if number2 := 3; number2 == 3 {
 		fmt.Println("Entro al condicional")
 	} 
+}
+
+func forTest() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("[FOR] ", i)
+	}
+
+	c := 100
+	for c > 0 {
+		c -= 10
+		fmt.Println("[FOR] Solo con una condicion de c > 0, ", c)
+	}
+
+	s := 1000
+	for {
+		s -= 1
+		if s == 0 {
+			fmt.Println("Termina el for 'infinito'")
+			break
+		}
+	}
 }
